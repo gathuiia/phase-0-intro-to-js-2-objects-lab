93051057a79917a934e37366f22b3b3778da83ee
Ian Gathui <ian.gathui@student.moringaschool.com>
2022-12-16 16:51:52 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name : "Ian",
+    streetAdress : "11 Westlands"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updEmployee = {...employee};
+    updEmployee[key] = value;
+    return updEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const delEmployee = {...employee};
+    delete delEmployee[key];
+    return delEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
